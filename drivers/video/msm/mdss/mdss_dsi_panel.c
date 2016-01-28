b16d481a970e5e22fa622034f19e3159e4e6d16b
franciscofranco <franciscofranco.1990@gmail.com>
2016-01-28 01:13:45 +0000

    display: add a simple api to query the display state (on/off) at any point in time
    Change-Id: Ic880224aada12d6f8b1097dd050a11eb103e77e9
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -23,6 +23,7 @@
 #include <linux/err.h>
 #include <linux/string.h>
 
+#include <linux/display_state.h>
 #include "mdss_dsi.h"
 
 #define DT_CMD_HDR 6
@@ -30,6 +31,26 @@
 #define DEFAULT_MDP_TRANSFER_TIME 14000
 
 DEFINE_LED_TRIGGER(bl_led_trigger);
+
+static int mdss_bl_ctrl_panel = false;
+static int bl_default_lvl = 1800;
+
+bool display_on = true;
+
+bool is_display_on(void)
+{
+        return display_on;
+}
+
+void mdss_set_bl_ctrl_by_panel(int enable)
+{
+	mdss_bl_ctrl_panel = enable;
+}
+
+static int mdss_bl_ctrl_by_panel(void)
+{
+	return mdss_bl_ctrl_panel;
+}
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
@@ -641,6 +662,8 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+	display_on = true;
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -690,6 +713,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 
 	if (ctrl->off_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds, CMD_REQ_COMMIT);
+
+	display_on = false;
 
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_BLANK;
